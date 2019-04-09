9432cc5252c628fa67e28965d5f54a3ad8e05837
Nae-Chyun <s93rm6@gmail.com>
2019-04-09 17:48:03 -0400

    first, updated this file for new data

---
+++ download_data.sh
@@ -0,0 +1,7 @@
+# Homo sapiens / K562 / ENCSR594NJP
+wget https://www.encodeproject.org/files/ENCFF297CNO/@@download/ENCFF297CNO.tsv
+wget https://www.encodeproject.org/files/ENCFF285HUZ/@@download/ENCFF285HUZ.tsv
+
+# Homo sapiens / GM12878 / ENCSR843RJV
+wget https://www.encodeproject.org/files/ENCFF853TRI/@@download/ENCFF853TRI.tsv
+wget https://www.encodeproject.org/files/ENCFF305QBE/@@download/ENCFF305QBE.tsv
