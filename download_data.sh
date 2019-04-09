d9bc2358f6ecea2793deecaa23cd41441ae0496b
Nae-Chyun <s93rm6@gmail.com>
2019-04-09 17:57:14 -0400

    added ENCFF879WBJ

--- download_data.sh
+++ download_data.sh
@@ -1,7 +1,11 @@
-# Homo sapiens / K562 / ENCSR594NJP
+## Homo sapiens / K562 / ENCSR594NJP
+# transcript quantifications
 wget https://www.encodeproject.org/files/ENCFF297CNO/@@download/ENCFF297CNO.tsv
 wget https://www.encodeproject.org/files/ENCFF285HUZ/@@download/ENCFF285HUZ.tsv
+# gene quantifications
+wget https://www.encodeproject.org/files/ENCFF879WBJ/@@download/ENCFF879WBJ.tsv
 
-# Homo sapiens / GM12878 / ENCSR843RJV
+## Homo sapiens / GM12878 / ENCSR843RJV
+# transcript quantifications
 wget https://www.encodeproject.org/files/ENCFF853TRI/@@download/ENCFF853TRI.tsv
 wget https://www.encodeproject.org/files/ENCFF305QBE/@@download/ENCFF305QBE.tsv
