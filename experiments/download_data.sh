6ba41be8cd15fbeaf245193e5915fd8e388e9c87
milkschen <s93rm6@gmail.com>
2019-07-18 15:13:36 -0400

    expr data preparation

---
+++ experiments/download_data.sh
@@ -0,0 +1,31 @@
+## K562 (Eric Lécuyer, IRCM)
+#wget https://www.encodeproject.org/files/ENCFF285HUZ/@@download/ENCFF285HUZ.tsv
+#wget https://www.encodeproject.org/files/ENCFF297CNO/@@download/ENCFF297CNO.tsv
+
+## K562 (Barbara Wold, Caltech)
+wget https://www.encodeproject.org/files/ENCFF425PIK/@@download/ENCFF425PIK.tsv
+wget https://www.encodeproject.org/files/ENCFF332TWQ/@@download/ENCFF332TWQ.tsv
+
+## GM12878 (Barbara Wold, Caltech)
+wget https://www.encodeproject.org/files/ENCFF853TRI/@@download/ENCFF853TRI.tsv
+wget https://www.encodeproject.org/files/ENCFF305QBE/@@download/ENCFF305QBE.tsv
+
+## HeLa-S2 (Barbara Wold, Caltech)
+wget https://www.encodeproject.org/files/ENCFF504AOD/@@download/ENCFF504AOD.tsv
+wget https://www.encodeproject.org/files/ENCFF214ZYK/@@download/ENCFF214ZYK.tsv
+
+## HepG2 (Barbara Wold, Caltech)
+wget https://www.encodeproject.org/files/ENCFF430KOC/@@download/ENCFF430KOC.tsv
+wget https://www.encodeproject.org/files/ENCFF966SVU/@@download/ENCFF966SVU.tsv
+
+## H1 (Barbara Wold, Caltech)
+wget https://www.encodeproject.org/files/ENCFF289GEC/@@download/ENCFF289GEC.tsv
+wget https://www.encodeproject.org/files/ENCFF679BHK/@@download/ENCFF679BHK.tsv
+
+## HUVEC (Barbara Wold, Caltech)
+wget https://www.encodeproject.org/files/ENCFF318QVC/@@download/ENCFF318QVC.tsv
+wget https://www.encodeproject.org/files/ENCFF211SQP/@@download/ENCFF211SQP.tsv
+
+## NHEK (Thomas Gingeras, CSHL)
+wget https://www.encodeproject.org/files/ENCFF566XEO/@@download/ENCFF566XEO.tsv
+wget https://www.encodeproject.org/files/ENCFF570MJL/@@download/ENCFF570MJL.tsv
