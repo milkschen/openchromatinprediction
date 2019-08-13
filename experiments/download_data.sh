321cc485e350fca216861bee69e7883a940c6311
arun96 <adas21@jhu.edu>
2019-08-12 23:58:41 -0400

    Updated download_data.sh with all cell lines

--- experiments/download_data.sh
+++ experiments/download_data.sh
@@ -29,3 +29,204 @@ wget https://www.encodeproject.org/files/ENCFF211SQP/@@download/ENCFF211SQP.tsv
 ## NHEK (Thomas Gingeras, CSHL)
 wget https://www.encodeproject.org/files/ENCFF566XEO/@@download/ENCFF566XEO.tsv
 wget https://www.encodeproject.org/files/ENCFF570MJL/@@download/ENCFF570MJL.tsv
+
+## A549 (Thomas Gingeras, CSHL)
+wget https://www.encodeproject.org/files/ENCFF578YTC/@@download/ENCFF578YTC.tsv
+wget https://www.encodeproject.org/files/ENCFF908XTR/@@download/ENCFF908XTR.tsv
+
+## H1-hESC (Barbara Wold, Caltech)
+wget https://www.encodeproject.org/files/ENCFF289GEC/@@download/ENCFF289GEC.tsv
+wget https://www.encodeproject.org/files/ENCFF466EBZ/@@download/ENCFF466EBZ.tsv
+
+## HMEC (Thomas Gingeras, CSHL)
+wget https://www.encodeproject.org/files/ENCFF749LVX/@@download/ENCFF749LVX.tsv
+wget https://www.encodeproject.org/files/ENCFF476ARS/@@download/ENCFF476ARS.tsv
+
+## HSMM (Thomas Gingeras, CSHL)
+wget https://www.encodeproject.org/files/ENCFF226JIS/@@download/ENCFF226JIS.tsv
+wget https://www.encodeproject.org/files/ENCFF036QPG/@@download/ENCFF036QPG.tsv
+
+## LNCaP - no Poly A plus RNA Seq
+
+## MCF-7 (Thomas Gingeras, CSHL)
+wget https://www.encodeproject.org/files/ENCFF735QLM/@@download/ENCFF735QLM.tsv
+wget https://www.encodeproject.org/files/ENCFF317XCR/@@download/ENCFF317XCR.tsv
+
+## Th1 (John Stamatoyannopoulos, UW)
+wget https://www.encodeproject.org/files/ENCFF879KHB/@@download/ENCFF879KHB.tsv
+wget https://www.encodeproject.org/files/ENCFF979WIG/@@download/ENCFF979WIG.tsv
+
+## AG04449 - no Poly A plus RNA Seq
+
+## AG09309 - no Poly A plus RNA Seq
+
+## AG09319 - no Poly A plus RNA Seq
+
+## AoAF - no Poly A plus RNA Seq
+
+## BE2C (Richard Myers, HAIB)
+wget https://www.encodeproject.org/files/ENCFF242CZI/@@download/ENCFF242CZI.tsv
+wget https://www.encodeproject.org/files/ENCFF465LLS/@@download/ENCFF465LLS.tsv
+wget https://www.encodeproject.org/files/ENCFF256IPJ/@@download/ENCFF256IPJ.tsv
+
+## BJ (Thomas Gingeras, CSHL)
+wget https://www.encodeproject.org/files/ENCFF982HRR/@@download/ENCFF982HRR.tsv
+wget https://www.encodeproject.org/files/ENCFF967KIB/@@download/ENCFF967KIB.tsv
+wget https://www.encodeproject.org/files/ENCFF154BBN/@@download/ENCFF154BBN.tsv
+
+## Caco-2 - no Poly A plus RNA Seq
+
+## CMK - no Poly A plus RNA Seq
+
+## GM06990 - no Poly A plus RNA Seq
+
+## GM12864 - no Poly A plus RNA Seq
+
+## H7-hESC - no Poly A plus RNA Seq
+
+## HAc - no Poly A plus RNA Seq
+
+## HAEpiC - no Poly A plus RNA Seq
+
+## HCT-116 - no Poly A plus RNA Seq with transcript quantifications
+
+## HFF - no Poly A plus RNA Seq
+
+## HFF-Myc - no Poly A plus RNA Seq
+
+## HGF - no Poly A plus RNA Seq
+
+## HL-60 - no Poly A plus RNA Seq
+
+## HMF - no Poly A plus RNA Seq
+
+## HMVEC-dLy-Ad - no Poly A plus RNA Seq
+
+## HMVEC-LBl  - no Poly A plus RNA Seq
+
+## HNPCEpiC - no Poly A plus RNA Seq
+
+## HPAEC - no Poly A plus RNA Seq
+
+## HPAF - no Poly A plus RNA Seq
+
+## HPdLF - no Poly A plus RNA Seq
+
+## HRCEpiC - no Poly A plus RNA Seq
+
+## HRE - no Poly A plus RNA Seq
+
+## HRPEpiC - no Poly A plus RNA Seq
+
+## Jurkat (Richard Myers, HAIB)
+wget https://www.encodeproject.org/files/ENCFF799GFQ/@@download/ENCFF799GFQ.tsv
+wget https://www.encodeproject.org/files/ENCFF847SAP/@@download/ENCFF847SAP.tsv
+wget https://www.encodeproject.org/files/ENCFF226OYU/@@download/ENCFF226OYU.tsv
+
+## Monocytes-CD14+ (Thomas Gingeras, CSHL)
+wget https://www.encodeproject.org/files/ENCFF167IBQ/@@download/ENCFF167IBQ.tsv
+wget https://www.encodeproject.org/files/ENCFF857HLU/@@download/ENCFF857HLU.tsv
+wget https://www.encodeproject.org/files/ENCFF780XKG/@@download/ENCFF780XKG.tsv
+
+## NH-A - no Poly A plus RNA Seq
+
+## NHDF-Ad - no Poly A plus RNA Seq
+
+## NHLF (Thomas Gingeras, CSHL)
+wget https://www.encodeproject.org/files/ENCFF609ZJO/@@download/ENCFF609ZJO.tsv
+wget https://www.encodeproject.org/files/ENCFF400BDD/@@download/ENCFF400BDD.tsv
+
+## NT2-D1 - no Poly A plus RNA Seq
+
+## Panc1 (Richard Myers, HAIB)
+wget https://www.encodeproject.org/files/ENCFF921FTG/@@download/ENCFF921FTG.tsv
+wget https://www.encodeproject.org/files/ENCFF149WMH/@@download/ENCFF149WMH.tsv
+wget https://www.encodeproject.org/files/ENCFF249UJV/@@download/ENCFF249UJV.tsv
+
+## PrEC - no Poly A plus RNA Seq
+
+## RPTEC - no Poly A plus RNA Seq
+
+## SAEC - no Poly A plus RNA Seq
+
+## SKMC - no Poly A plus RNA Seq
+
+## SK-N-MC - no Poly A plus RNA Seq
+
+## SK-N-SH-RA - no Poly A plus RNA Seq
+
+## Th2 (John Stamatoyannopoulos, UW)
+wget https://www.encodeproject.org/files/ENCFF979WIG/@@download/ENCFF979WIG.tsv
+wget https://www.encodeproject.org/files/ENCFF879KHB/@@download/ENCFF879KHB.tsv
+
+## WERI-Rb-1 - no Poly A plus RNA Seq
+
+## WI38 - no Poly A plus RNA Seq
+
+## WI38-TAM - no Poly A plus RNA Seq
+
+## CD20 (Thomas Gingeras, CSHL)
+wget https://www.encodeproject.org/files/ENCFF642IQW/@@download/ENCFF642IQW.tsv
+wget https://www.encodeproject.org/files/ENCFF410IPV/@@download/ENCFF410IPV.tsv
+
+## CD34 (John Stamatoyannopoulos, UW)
+wget https://www.encodeproject.org/files/ENCFF040ZTX/@@download/ENCFF040ZTX.tsv
+wget https://www.encodeproject.org/files/ENCFF932WWC/@@download/ENCFF932WWC.tsv
+
+## Ishikawa-ES (Richard Myers, HAIB)
+wget https://www.encodeproject.org/files/ENCFF770CRK/@@download/ENCFF770CRK.tsv
+wget https://www.encodeproject.org/files/ENCFF667ZBZ/@@download/ENCFF667ZBZ.tsv
+
+## Ishikawa-TAM - no Poly A plus RNA Seq
+
+## 8988T - no Poly A plus RNA Seq
+
+## Chorion (Joseph Costello, UCSF)
+wget https://www.encodeproject.org/files/ENCFF954RVV/@@download/ENCFF954RVV.tsv
+wget https://www.encodeproject.org/files/ENCFF376HRQ/@@download/ENCFF376HRQ.tsv
+
+## GM12891 - no Poly A plus RNA Seq
+
+## GM12892 - no Poly A plus RNA Seq
+
+## GM18507 - no Poly A plus RNA Seq
+
+## GM19238 - no Poly A plus RNA Seq
+
+## GM19239 - no Poly A plus RNA Seq
+
+## GM19240 - no Poly A plus RNA Seq
+
+## H9ES - no Poly A plus RNA Seq
+
+## HeLa-S3 (Barbara Wold, Caltech)
+wget https://www.encodeproject.org/files/ENCFF255QOM/@@download/ENCFF255QOM.tsv
+wget https://www.encodeproject.org/files/ENCFF214ZYK/@@download/ENCFF214ZYK.tsv
+
+## Hepatocytes (Barbara Wold, Caltech)
+wget https://www.encodeproject.org/files/ENCFF264EOK/@@download/ENCFF264EOK.tsv
+wget https://www.encodeproject.org/files/ENCFF064IQR/@@download/ENCFF064IQR.tsv
+
+## HPDE6-E6E7 - no Poly A plus RNA Seq
+
+## HTR8svn - no Poly A plus RNA Seq
+
+## Huh-7.5 - no Poly A plus RNA Seq
+
+## Huh-7 - no Poly A plus RNA Seq
+
+## iPS - no Poly A plus RNA Seq
+
+## Medullo - no Poly A plus RNA Seq
+
+## Melano - no Poly A plus RNA Seq
+
+## osteoblast - no Poly A plus RNA Seq
+
+## stellate - no Poly A plus RNA Seq
+
+## T-47D (Richard Myers, HAIB)
+wget https://www.encodeproject.org/files/ENCFF549FIH/@@download/ENCFF549FIH.tsv
+wget https://www.encodeproject.org/files/ENCFF234TQB/@@download/ENCFF234TQB.tsv
+
+## urothelia - no Poly A plus RNA Seq
